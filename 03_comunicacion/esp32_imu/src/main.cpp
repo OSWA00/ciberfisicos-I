56ec361fa4f881af6054d03ce3740c2552c1122b
oswaldohernandez <oswaldohernandez@protonmail.com>
2022-09-05 17:40:40 -0500

    feat IMU lib

---
+++ 03_comunicacion/esp32_imu/src/main.cpp
@@ -0,0 +1,13 @@
+#include <IMU.h>
+
+#define DT 0.02
+#define AA 0.97
+#define G_GAIN 0.070
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
